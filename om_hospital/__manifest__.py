93fc829faf01b337bbe5e4d50169a05470c60eb0
Nikhil Mudholkar <nikhilrmudholkar29@gmail.com>
2023-07-27 11:01:03 +0200

    first commit

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Hospital Management System Bryo test',
+    'version': '1.0',
+    'category': 'Extra Tools',
+    'sequence': '-100',
+    'description': """Hospital Management System for testing for Bryo UG""",
+    'author': 'Bryo UG',
+    'maintainer': 'Bryo UG',
+    'license': 'LGPL-3',
+    'website': 'https://www.bryo.io',
+    'summary': 'Odoo 16 developement test module',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient.xml',
+
+    ]
+}
